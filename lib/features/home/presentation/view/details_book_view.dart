5404f6c216a2e891dc4d98f08afd5f3c02c0473b
mohmed <mohmedhty1@gmail.com>
2023-08-09 07:38:06 +0300

    add buttons in screen details

--- lib/features/home/presentation/view/details_book_view.dart
+++ lib/features/home/presentation/view/details_book_view.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/view/widgets/book_rating.dart';
 import 'package:bookly/features/home/presentation/view/widgets/custom_details_view_app_bar.dart';
+import 'package:bookly/features/home/presentation/view/widgets/details_view_button.dart';
 import 'package:bookly/features/home/presentation/view/widgets/top_list_view_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -48,7 +49,42 @@ class DetailsBookView extends StatelessWidget {
             SizedBox(
               height: 14.h,
             ),
-            const BookRating()
+            const BookRating(),
+            const SizedBox(
+              height: 40,
+            ),
+
+            const Row(
+
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                DetailsViewButton(
+
+                  text: '19.99 €',
+                  fontColor: Colors.black,
+                  fontSize: 18,
+                  backGroundColor: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      bottomLeft: Radius.circular(16)
+                  ),
+
+                ),
+
+                DetailsViewButton(
+
+                  text: 'Free preview',
+                  fontColor: Colors.white,
+                  fontSize: 16,
+                  backGroundColor: Color(0xffEF8262),
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(16),
+                      bottomRight: Radius.circular(16)
+                  ),
+
+                )
+              ],
+            )
           ],
         ),
       ),
---
+++ lib/features/home/presentation/view/widgets/details_view_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DetailsViewButton extends StatelessWidget {
+  const DetailsViewButton(
+      {super.key,
+      required this.fontColor,
+      required this.backGroundColor,
+      required this.fontSize,
+      required this.borderRadius,
+      required this.text});
+
+  final Color fontColor;
+  final Color backGroundColor;
+  final double fontSize;
+  final BorderRadius borderRadius;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SizedBox(
+        height: 55.h,
+        child: TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+              backgroundColor: backGroundColor,
+              shadowColor: Colors.black,
+              shape: RoundedRectangleBorder(borderRadius: borderRadius)),
+          child: Text(
+            text,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: fontColor,
+                fontSize: fontSize),
+          ),
+        ),
+      ),
+    );
+  }
+}
