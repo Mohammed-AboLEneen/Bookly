ded210d7fbbe68ea549126ea8d20ecc3beba5611
mohmed <mohmedhty1@gmail.com>
2023-08-11 05:00:16 +0300

    add book model and repository class and fetch method

--- lib/features/home/data/models/book_model/AccessInfo.dart
+++ lib/features/home/data/models/book_model/AccessInfo.dart
@@ -1,58 +1,111 @@
 import 'Epub.dart';
 import 'Pdf.dart';
 
+/// country : "US"
+/// viewability : "NO_PAGES"
+/// embeddable : false
+/// publicDomain : false
+/// textToSpeechPermission : "ALLOWED"
+/// epub : {"isAvailable":false}
+/// pdf : {"isAvailable":false}
+/// webReaderLink : "http://play.google.com/books/reader?id=BOyCSAAACAAJ&hl=&source=gbs_api"
+/// accessViewStatus : "NONE"
+/// quoteSharingAllowed : false
+
 class AccessInfo {
   AccessInfo({
-      this.country, 
-      this.viewability, 
-      this.embeddable, 
-      this.publicDomain, 
-      this.textToSpeechPermission, 
-      this.epub, 
-      this.pdf, 
-      this.webReaderLink, 
-      this.accessViewStatus, 
-      this.quoteSharingAllowed,});
+      String? country, 
+      String? viewability, 
+      bool? embeddable, 
+      bool? publicDomain, 
+      String? textToSpeechPermission, 
+      Epub? epub, 
+      Pdf? pdf, 
+      String? webReaderLink, 
+      String? accessViewStatus, 
+      bool? quoteSharingAllowed,}){
+    _country = country;
+    _viewability = viewability;
+    _embeddable = embeddable;
+    _publicDomain = publicDomain;
+    _textToSpeechPermission = textToSpeechPermission;
+    _epub = epub;
+    _pdf = pdf;
+    _webReaderLink = webReaderLink;
+    _accessViewStatus = accessViewStatus;
+    _quoteSharingAllowed = quoteSharingAllowed;
+}
 
   AccessInfo.fromJson(dynamic json) {
-    country = json['country'];
-    viewability = json['viewability'];
-    embeddable = json['embeddable'];
-    publicDomain = json['publicDomain'];
-    textToSpeechPermission = json['textToSpeechPermission'];
-    epub = json['epub'] != null ? Epub.fromJson(json['epub']) : null;
-    pdf = json['pdf'] != null ? Pdf.fromJson(json['pdf']) : null;
-    webReaderLink = json['webReaderLink'];
-    accessViewStatus = json['accessViewStatus'];
-    quoteSharingAllowed = json['quoteSharingAllowed'];
+    _country = json['country'];
+    _viewability = json['viewability'];
+    _embeddable = json['embeddable'];
+    _publicDomain = json['publicDomain'];
+    _textToSpeechPermission = json['textToSpeechPermission'];
+    _epub = json['epub'] != null ? Epub.fromJson(json['epub']) : null;
+    _pdf = json['pdf'] != null ? Pdf.fromJson(json['pdf']) : null;
+    _webReaderLink = json['webReaderLink'];
+    _accessViewStatus = json['accessViewStatus'];
+    _quoteSharingAllowed = json['quoteSharingAllowed'];
   }
-  String country;
-  String viewability;
-  bool embeddable;
-  bool publicDomain;
-  String textToSpeechPermission;
-  Epub epub;
-  Pdf pdf;
-  String webReaderLink;
-  String accessViewStatus;
-  bool quoteSharingAllowed;
+  String? _country;
+  String? _viewability;
+  bool? _embeddable;
+  bool? _publicDomain;
+  String? _textToSpeechPermission;
+  Epub? _epub;
+  Pdf? _pdf;
+  String? _webReaderLink;
+  String? _accessViewStatus;
+  bool? _quoteSharingAllowed;
+AccessInfo copyWith({  String? country,
+  String? viewability,
+  bool? embeddable,
+  bool? publicDomain,
+  String? textToSpeechPermission,
+  Epub? epub,
+  Pdf? pdf,
+  String? webReaderLink,
+  String? accessViewStatus,
+  bool? quoteSharingAllowed,
+}) => AccessInfo(  country: country ?? _country,
+  viewability: viewability ?? _viewability,
+  embeddable: embeddable ?? _embeddable,
+  publicDomain: publicDomain ?? _publicDomain,
+  textToSpeechPermission: textToSpeechPermission ?? _textToSpeechPermission,
+  epub: epub ?? _epub,
+  pdf: pdf ?? _pdf,
+  webReaderLink: webReaderLink ?? _webReaderLink,
+  accessViewStatus: accessViewStatus ?? _accessViewStatus,
+  quoteSharingAllowed: quoteSharingAllowed ?? _quoteSharingAllowed,
+);
+  String? get country => _country;
+  String? get viewability => _viewability;
+  bool? get embeddable => _embeddable;
+  bool? get publicDomain => _publicDomain;
+  String? get textToSpeechPermission => _textToSpeechPermission;
+  Epub? get epub => _epub;
+  Pdf? get pdf => _pdf;
+  String? get webReaderLink => _webReaderLink;
+  String? get accessViewStatus => _accessViewStatus;
+  bool? get quoteSharingAllowed => _quoteSharingAllowed;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
-    map['country'] = country;
-    map['viewability'] = viewability;
-    map['embeddable'] = embeddable;
-    map['publicDomain'] = publicDomain;
-    map['textToSpeechPermission'] = textToSpeechPermission;
-    if (epub != null) {
-      map['epub'] = epub.toJson();
+    map['country'] = _country;
+    map['viewability'] = _viewability;
+    map['embeddable'] = _embeddable;
+    map['publicDomain'] = _publicDomain;
+    map['textToSpeechPermission'] = _textToSpeechPermission;
+    if (_epub != null) {
+      map['epub'] = _epub?.toJson();
     }
-    if (pdf != null) {
-      map['pdf'] = pdf.toJson();
+    if (_pdf != null) {
+      map['pdf'] = _pdf?.toJson();
     }
-    map['webReaderLink'] = webReaderLink;
-    map['accessViewStatus'] = accessViewStatus;
-    map['quoteSharingAllowed'] = quoteSharingAllowed;
+    map['webReaderLink'] = _webReaderLink;
+    map['accessViewStatus'] = _accessViewStatus;
+    map['quoteSharingAllowed'] = _quoteSharingAllowed;
     return map;
   }
 
